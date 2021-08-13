f89a2add311680cf258f2651fb73a38c811466e4
LisaHackett97 <lisa-hackett@hotmail.com>
2021-08-13 07:45:09 +0000

    Lessons for sql cmds

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os
+# import datetime
 import pymysql
 
 username= os.getenv('C9_USER')
@@ -10,12 +11,56 @@ connection = pymysql.connect(host = 'localhost',
 
                             
 try:
+    # run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        list_of_names = ['Jim', 'Bob', 'Fred']
+        # prepare a str with same number of placeholders as in list_of_names
+        format_strings = ','.join(['%s'] * len(list_of_names))
+        cursor.execute("DELETE FROM Friends WHERE name in ({});".format(format_strings), list_of_names)
+        connection.commit()
 finally:
     connection.close()
+
+# Code for lessons up to CREAT table
+#     try:
+#     # with connection.cursor() as cursor:
+#     with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+#         # sql = "SELECT * FROM Artist;"
+#         cursor.execute("""CREATE TABLE IF NOT EXISTS
+#                         Friends(name char(20), age int, DOB datetime)""")
+#         # result = cursor.fetchall()
+#         for row in cursor:
+#             print(row)
+#         # print(result)
+# finally:
+#     connection.close()
+
+# Lesson for INSERTING
+
+                            
+# try:
+#     # run a query
+#     with connection.cursor() as cursor:
+#         row = [("Bob", 21, "1990-02-06 23:04:56"),
+#                 ("Jim", 56, "1955-05-09 13:12:45"),
+#                 ("Fred", 100, "1911-09-12 01:01:01")]
+#         cursor.executemany("INSERT INTO Friends values (%s, %s, %s);", row)
+#         connection.commit()
+# finally:
+#     connection.close()
+
+# Lessons for update, execute many
+# try:
+#     # run a query
+#     with connection.cursor() as cursor:
+#         rows = [(23, 'Bob'),
+#                 (24, 'Jim'),
+#                 (25, 'Fred')]
+#         cursor.executemany("UPDATE Friends set AGE = %s WHERE name = %s;",
+#         rows)
+#         connection.commit()
+# finally:
+#     connection.close()
+
 
 
